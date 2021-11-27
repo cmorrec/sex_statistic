76f545a45c56a93ad116064babe91a1395cc9590
warlikegh <a_katnov@mail.ru>
2021-11-27 15:04:45 +0300

    set login and password saveable

--- app/src/main/java/com/zzz/sexstatistic/presentation/auth/SignIn.kt
+++ app/src/main/java/com/zzz/sexstatistic/presentation/auth/SignIn.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -19,8 +20,8 @@ import com.zzz.sexstatistic.presentation.theme.SexStatisticTheme
 fun SignIn(
     authViewModel: AuthViewModel = hiltViewModel(),
 ) {
-    val (login, setLogin) = remember { mutableStateOf("") }
-    val (password, setPassword) = remember { mutableStateOf("") }
+    val (login, setLogin) = rememberSaveable { mutableStateOf("") }
+    val (password, setPassword) = rememberSaveable { mutableStateOf("") }
     val status by authViewModel.status.collectAsState()
     when (status) {
         ActionStatus.LOADING -> Text("Loading")
